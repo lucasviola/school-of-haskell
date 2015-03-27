e1059c48def80b148ef4fd1f771385ca4b72b83e
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-26 23:03:17 -0300

    *

--- haskell-fast-hard/evenSum-v1.hs
+++ haskell-fast-hard/evenSum-v1.hs
@@ -1,13 +1,13 @@
+-- Version 1
 evenSum :: [Integer] -> Integer
 
 evenSum l = accumSum 0 l
 
 accumSum n l = if l == []
-				   then n
-				   else let x = head l
-				   			xs = tail l
-				   	    in if even x
-				   	    	  then accumSum (n+xs) xs
-				   	    	  else accumSum n xs
+                  then n
+                  else let x = head l
+                           xs = tail l
+                       in if even x
+                              then accumSum (n+x) xs
+                              else accumSum n xs
 main = print $ evenSum [1..10] 
-
