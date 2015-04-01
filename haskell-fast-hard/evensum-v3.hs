75abf9442e9278624aaddbb02a6aae42598ed4f8
lucasviola <lucasmatzenbacher@gmail.com>
2015-03-31 21:05:54 -0300

    usando pattern matching

---
+++ haskell-fast-hard/evensum-v3.hs
@@ -0,0 +1,9 @@
+-- Versão 3: Usando Pattern Matching
+
+evenSum l = accumSum 0 l
+    where
+        accumSum n [] = n
+        accumSum n (x:xs) =
+             if even x
+                then accumSum (n+x) xs
+                else accumSum n xs
