67eee4dc5ed9517e5d25aa8017906bc042360269
lucasviola <lucasmatzenbacher@gmail.com>
2015-04-09 09:05:06 -0300

    todos exercicios resolvidos

--- basics-of-haskell/toInts.hs
+++ basics-of-haskell/toInts.hs
@@ -4,5 +4,8 @@ toInts :: String -> [Int]
 toInts [] = []
 toInts (x:xs) = digitToInt x : toInts xs
 
+sumList :: String -> Int
+sumList = sum . toInts
+-- sumList (x:xs) = toInts xs $ sumList x + sumList xs
 
-main = print $ toInts "2013"
+main = print $ sumList "2013"
