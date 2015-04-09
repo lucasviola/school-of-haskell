74cea58d8a4219d735ef3b358cc8c84ae00826a2
lucasviola <lucasmatzenbacher@gmail.com>
2015-04-09 08:19:09 -0300

    primeira versão do tokenizer pronta

--- basics-of-haskell/symbolic-calculator.hs
+++ basics-of-haskell/symbolic-calculator.hs
@@ -16,12 +16,20 @@ operatorToString Minus = "-"
 operatorToString Times = "*"
 operatorToString Div = "/"
 
-charToOperator :: Char -> Maybe Operator
-charToOperator c | c == '+' = Just Plus
-           		 | c == '-' = Just Minus
-           		 | c == '*' = Just Times
-           		 | c == '/' = Just Div
-           		 | otherwise = Nothing
+charToOperator :: Char -> Operator
+charToOperator c | c == '+' = Plus
+           		 | c == '-' = Minus
+           		 | c == '*' = Times
+           		 | c == '/' = Div
+
+tokenize :: String -> [Token]
+tokenize [] = []
+tokenize (c:cs)
+	| elem c "+-*/" = TokenOperator (charToOperator c) : tokenize cs
+	| isDigit c = TokenNumber (digitToInt c) : tokenize cs
+	| isAlpha c = TokenIdentifier [c] 		 : tokenize cs
+	| isSpace c = tokenize cs
+	| otherwise = error $ "Cannot tokenize: " ++ [c]
 
 showContent :: Token -> String
 showContent (TokenOperator operator) = operatorToString operator
@@ -38,7 +46,7 @@ token :: Token
 token = TokenIdentifier "x"
 
 main :: IO ()
-main = do print $ charToOperator '1'
+main = do print $ tokenize "1 + 4 / x"
 	
 --	line <- getLine
 --	putStrLn line
